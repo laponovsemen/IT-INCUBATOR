70a638e914125b52a55bd706f8db3fcf9b1c3825
laponovsemen <126752268+laponovsemen@users.noreply.github.com>
2023-03-16 18:24:02 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+/* 
+First server
+*/
+
+------------------------------------------------------------------------------------------------------------------------------
+
+const http = require("http")
+let requestsCount = 0
+const server = http.createServer((request , response) => {
+    requestsCount++
+    switch (request.url) {
+        case "/students":
+            response.write("STUDENTS");
+            break;
+        case "/courses":
+            response.write("FRONT + BACK");
+            break;
+        default:
+            response.write("404 not found");
+
+    }
+    //response.write("It-kamasutra " + requestsCount)
+    response.end()
+})
+
+server.listen(3003)
